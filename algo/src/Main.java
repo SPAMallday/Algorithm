18418e5f90b108b1c8e4c6125e8086e1b1a5cc91
SPAMallday <pjw090@gmail.com>
2023-11-23 14:07:10 +0900

    백준 16561 3의 배수

---
+++ algo/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// bj 16561
+public class Main {
+    public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int tc = Integer.parseInt(st.nextToken());
+        
+        int total = tc / 3;
+
+        if (total < 3) {
+            sb.append(0);
+        }
+        else {
+            int sum = 0;
+
+            for (int i = 1; i <= total-2; i++) {
+                sum += i;
+            }
+
+            sb.append(sum);
+        }
+
+        System.out.println(sb);
+    }
+}
